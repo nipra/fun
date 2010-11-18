93b1923070bf8534332708be2d066842f0ff2a3a
Nikhil Prabhakar <prabhakar.nikhil@gmail.com>
2010-11-19 04:49:20 +0530

    * Added some admin util functions for postgres.

--- src/pg/utils.clj
+++ src/pg/utils.clj
@@ -3,22 +3,64 @@
             [clojure.contrib.sql [internal :as internal]]
             [clojure.pprint :as p]
             [pg.main :as main]
-            [clojureql.core :as cql])
+            [clojureql.core :as cql]
+            [clj-time [core :as ctc] [coerce :as ctco] [format :as ctf]])
   (:import [java.sql Connection DriverManager ResultSet SQLException
             Statement ResultSetMetaData]
            [org.postgresql Driver]))
 
-
+;;; 
 (defn get-databases
   [db]
   (sql/with-connection db
     (sql/with-query-results res
-      ["select datname from pg_database;"]
+      ["SELECT datname FROM pg_database;"]
       (sort (map :datname (vec res))))))
 
 (defn get-databases+
   [db]
-  (let [cur (cql/table db :pg_database)
-        res @(-> (cql/select cur true)
+  (let [robj (cql/table db :pg_database)
+        res @(-> (cql/select robj true)
                  (cql/project #{:datname}))]
     (sort (map :datname res))))
+
+;;; 
+(defn server-uptime
+  [db]
+  (sql/with-connection db
+    (sql/with-query-results res
+      ["SELECT date_trunc('second', (current_timestamp - pg_postmaster_start_time())) as uptime"]
+      (:uptime (first (vec res))))))
+
+;;; 
+(defn get-no-of-tables
+  [db]
+  (sql/with-connection db
+    (sql/with-query-results res
+      ["SELECT count(*) FROM information_schema.tables
+        WHERE table_schema NOT IN ('information_schema', 'pg_catalog')"]
+      (vec res))))
+
+(defn get-no-of-tables+
+  [db]
+  (let [robj (cql/table db :information_schema.tables)]
+    @(-> (cql/select robj (cql/where "table_schema NOT IN ('information_schema', 'pg_catalog')"))
+         (cql/aggregate [:count/*]))))
+
+(defn get-no-of-tables*
+  [db]
+  (let [robj (cql/table db :information_schema.tables)]
+    @(-> (cql/select robj (cql/where (and (!= :table_schema "information_schema")
+                                          (!= :table_schema "pg_catalog"))))
+         (cql/aggregate [:count/*]))))
+
+;;;
+(defn get-tables+
+  [db]
+  (let [robj (cql/table db :information_schema.tables)
+        res @(-> (cql/select robj (cql/where (and (!= :table_schema "information_schema")
+                                                  (!= :table_schema "pg_catalog"))))
+                 (cql/project #{:table_name}))]
+    (sort (map :table_name res))))
+
+;;;
