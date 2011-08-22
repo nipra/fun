95e2219d261e19c26fb5b624d7a186944d1d776a
Nikhil Prabhakar <prabhakar.nikhil@gmail.com>
2011-08-23 00:23:03 +0530

    * Conflict.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj1.2 "1.0.0-SNAPSHOT"
+(defproject fun "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
@@ -15,7 +15,7 @@
                  [org.apache.lucene/lucene-highlighter "3.0.3"]
                  [clj-http "0.1.3"]
                  [javax.mail/mail "1.4.1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
+  :dev-dependencies [[swank-clojure "1.3.2"]
                      [lein-clojars "0.6.0"]
                      [lein-run "1.0.0"]]
   :repositories {"clojars" "http://clojars.org/repo"
